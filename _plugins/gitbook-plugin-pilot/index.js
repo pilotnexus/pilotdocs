27daad43750fcefd5a100876eeb81ccb1e1f21b4
Daniel Amesberger <daniel.amesberger@amescon.com>
2018-08-28 21:11:59 +0200

    moved module logic to plugin

--- _plugins/gitbook-plugin-pilot/index.js
+++ _plugins/gitbook-plugin-pilot/index.js
@@ -2,9 +2,16 @@
  * @param {String} html
  * @return {String} HTML wrapped in a hint container
  */
+
+// var moduletemplate = require('./module-template.html');
+
 function wrapInContainer(html) {
     return '<div class="hints-container">'+html+'</div>';
 }
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
 
 module.exports = {
     book: {
@@ -24,7 +31,7 @@ module.exports = {
                 return this
                     .renderBlock('markdown', block.body)
                     .then(function(renderedBody) {
-                        return '<div>this is my plugin</div>';
+                        return renderedBody + fs.readFileSync(path.join(__dirname, '/module-template.html'), 'utf-8').replace('{{svg}}',svg);
                     });
             }
         }
